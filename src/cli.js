840047efa6228e6b1327b349fb8bd3c474ee217d
Yana Pavelchuk <yapavelchuk@yandex.ru>
2022-11-24 13:42:46 +0500

    update brain-games.js and cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const greeting = () => {
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}!`);
+const greeting = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
 
+export default greeting;
