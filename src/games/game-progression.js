4b9c2d0caf793aef8e2af1f2c8c2a5e61181901a
Yana Pavelchuk <yapavelchuk@yandex.ru>
2022-12-14 02:59:46 +0500

    add game-progression

---
+++ src/games/game-progression.js
@@ -0,0 +1,18 @@
+import getRandomNumber from '../utils.js';
+import gameLogic from '../index.js';
+
+const gameQuestion = 'What number is missing in the progression?';
+const generateGame = () => {
+  const givenNumber1 = getRandomNumber(20);
+  const givenNumber2 = getRandomNumber(10);
+  const progression = [];
+  const hiddenElement = getRandomNumber(givenNumber1, (progression.length - 1));
+  for (let i = givenNumber1; progression.length <= 9; i += givenNumber2) {
+    progression.push(i);
+  }
+  const correctAnswer = progression[hiddenElement];
+  progression[hiddenElement] = '...';
+  return [progression.join(), String(correctAnswer)];
+};
+
+export default () => gameLogic(gameQuestion, generateGame);
