127a8a5d31bb0d9e4d785bca00cd353c9c17ebc1
Yana Pavelchuk <yapavelchuk@yandex.ru>
2022-12-16 22:14:48 +0500

    change game-calc

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -2,30 +2,32 @@ import getRandomNumber from '../utils.js';
 import gameLogic from '../index.js';
 
 const gameRule = 'What is the result of the expression?';
+const calculateExpressionValue = (number1, operator, number2) => {
+  const mathOperations = ['+', '-', '*'];
+  mathOperations[getRandomNumber(0, 2)] = operator;
+  let resultOfExpression;
+  switch (operator) {
+    case '+':
+      resultOfExpression = number1 + number2;
+      break;
+    case '-':
+      resultOfExpression = number1 - number2;
+      break;
+    case '*':
+      resultOfExpression = number1 * number2;
+      break;
+    default:
+      throw new Error(`Unknown order state: '${operator}'!`);
+  }
+  return resultOfExpression;
+};
 const generateRoundData = () => {
   const mathOperations = ['+', '-', '*'];
   const operator = mathOperations[getRandomNumber(0, 2)];
-  const calculateExpressionValue = (number1, number2) => {
-    let resultOfExpression;
-    switch (operator) {
-      case '+':
-        resultOfExpression = number1 + number2;
-        break;
-      case '-':
-        resultOfExpression = number1 - number2;
-        break;
-      case '*':
-        resultOfExpression = number1 * number2;
-        break;
-      default:
-        throw new Error(`Unknown order state: '${operator}'!`);
-    }
-    return resultOfExpression;
-  };
   const givenNumber1 = getRandomNumber();
   const givenNumber2 = getRandomNumber();
   const question = `${givenNumber1} ${operator} ${givenNumber2}`;
-  const result = calculateExpressionValue(givenNumber1, givenNumber2);
+  const result = calculateExpressionValue(givenNumber1, operator, givenNumber2);
   return [question, String(result)];
 };
 
