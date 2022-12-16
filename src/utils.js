06e104497650fddd4821804cf9d6e8864d08ff60
Yana Pavelchuk <yapavelchuk@yandex.ru>
2022-12-16 23:26:38 +0500

    update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
-const getRandomNumber = (min = 0, max = 10) => Math.round(Math.random() * (max - min + 1)) + min;
+const getRandomNumber = (min = 0, max = 10) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 export default getRandomNumber;
