ba22ed8201cf803f75129e064af07aeaca7b28fa
Yana Pavelchuk <yapavelchuk@yandex.ru>
2022-12-19 14:28:46 +0500

    delete comments in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import readlineSync from 'readline-sync';
 
-const gameLogic = (gameRule, generateRoundData) => {
+const runTheGame = (gameRule, generateDataforNextRound) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(gameRule);
   const maxAttempts = 3;
   for (let i = 1; i <= maxAttempts; i += 1) {
-    const [question, correctAnswer] = generateRoundData();
+    const [question, correctAnswer] = generateDataforNextRound();
     console.log('Question:', question);
     const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer === correctAnswer) {
@@ -16,10 +16,9 @@ const gameLogic = (gameRule, generateRoundData) => {
       console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
       console.log(`Let's try again, ${userName}!`);
       return;
-      // завершить игру
     }
   }
   console.log(`Congratulations, ${userName}!`);
 };
 
-export default gameLogic;
+export default runTheGame;
