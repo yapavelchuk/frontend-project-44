d9b901eb04cd6a3c336d80b64b3fe6e24e98c5f8
Yana Pavelchuk <yapavelchuk@yandex.ru>
2022-12-19 15:00:45 +0500

    update game-even

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -2,16 +2,11 @@ import getRandomNumber from '../utils.js';
 import runTheGame from '../index.js';
 
 const gameRule = 'Answer "yes" if the number is even, otherwise answer "no".';
-const isEven = (number) => {
-  if (number % 2 === 0) {
-    return true;
-  }
-  return false;
-};
+const isEven = (number) => ((number % 2 === 0) ? 'yes' : 'no');
+
 const generateDataforNextRound = () => {
   const givenNumber = getRandomNumber();
-  const sayEvenOrNot = (number) => (isEven(number) ? 'yes' : 'no');
-  const correctAnswer = sayEvenOrNot(givenNumber);
+  const correctAnswer = isEven(givenNumber);
   return [givenNumber, correctAnswer];
 };
 
